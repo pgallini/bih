d149c89e8432184d8c711ea00d8bf0e4ffdd55a3
ase408 <paulm.gallini@capitalone.com>
2017-10-29 18:12:33 -0400

    Added ads to main and fixed logic to enforce Free logic

---
+++ src/main/java/common/BuildConfigHelper.java
@@ -0,0 +1,16 @@
+package common;
+
+import com.birdinhand.funkynetsoftware.BuildConfig;
+
+/**
+ * Created by ase408 on 10/29/17.
+ */
+
+public final class BuildConfigHelper {
+    public static final boolean DEBUG = BuildConfig.DEBUG;
+    public static final String APPLICATION_ID = BuildConfig.APPLICATION_ID;
+    public static final String BUILD_TYPE = BuildConfig.BUILD_TYPE;
+    public static final String FLAVOR = BuildConfig.FLAVOR;
+    public static final int VERSION_CODE = BuildConfig.VERSION_CODE;
+    public static final String VERSION_NAME = BuildConfig.VERSION_NAME;
+}
